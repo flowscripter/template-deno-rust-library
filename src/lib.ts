401227f87d9935a4b566840fc48db7f07a966941
vectronic <hello.vectronic@gmail.com>
2022-06-08 23:34:55 +0100

    style: minor tweaks (#32)

--- src/lib.ts
+++ src/lib.ts
@@ -2,7 +2,7 @@ import { add } from "../bindings/bindings.ts";
 import { log } from "../deps.ts";
 
 /**
- * Adds 2 and 2 and logs the result as "World (4)"
+ * Adds 2 and 2 and logs the result as "World 4"
  */
 export function world(): void {
   log.info(`World ${add(2, 2)}`);
