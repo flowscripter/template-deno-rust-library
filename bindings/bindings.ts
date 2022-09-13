4eba7afa6519039eede2aaf5282898b77f69f8e7
vectronic <hello.vectronic@gmail.com>
2022-09-13 12:47:32 +0100

    fix: use patched deno_bindgen with better solution to support docs.rs (#55)

--- bindings/bindings.ts
+++ bindings/bindings.ts
@@ -1,31 +1,31 @@
 // Auto-generated with deno_bindgen
-import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.2/plug.ts";
+import { CachePolicy, prepare } from "https://deno.land/x/plug@0.5.2/plug.ts"
 
 function encode(v: string | Uint8Array): Uint8Array {
-  if (typeof v !== "string") return v;
-  return new TextEncoder().encode(v);
+  if (typeof v !== "string") return v
+  return new TextEncoder().encode(v)
 }
 
 function decode(v: Uint8Array): string {
-  return new TextDecoder().decode(v);
+  return new TextDecoder().decode(v)
 }
 
 function readPointer(v: any): Uint8Array {
-  const ptr = new Deno.UnsafePointerView(v as bigint);
-  const lengthBe = new Uint8Array(4);
-  const view = new DataView(lengthBe.buffer);
-  ptr.copyInto(lengthBe, 0);
-  const buf = new Uint8Array(view.getUint32(0));
-  ptr.copyInto(buf, 4);
-  return buf;
+  const ptr = new Deno.UnsafePointerView(v as bigint)
+  const lengthBe = new Uint8Array(4)
+  const view = new DataView(lengthBe.buffer)
+  ptr.copyInto(lengthBe, 0)
+  const buf = new Uint8Array(view.getUint32(0))
+  ptr.copyInto(buf, 4)
+  return buf
 }
 
-const url = new URL("../target/debug", import.meta.url);
-let uri = url.toString();
-if (!uri.endsWith("/")) uri += "/";
+const url = new URL("../target/debug", import.meta.url)
+let uri = url.toString()
+if (!uri.endsWith("/")) uri += "/"
 
-let darwin: string | { aarch64: string; x86_64: string } = uri +
-  "libflowscripter_template_deno_rust_library.dylib";
+let darwin: string | { aarch64: string; x86_64: string } = uri
+  + "libflowscripter_template_deno_rust_library.dylib"
 
 if (url.protocol !== "file:") {
   // Assume that remote assets follow naming scheme
@@ -33,7 +33,7 @@ if (url.protocol !== "file:") {
   darwin = {
     aarch64: uri + "libflowscripter_template_deno_rust_library_arm64.dylib",
     x86_64: uri + "libflowscripter_template_deno_rust_library.dylib",
-  };
+  }
 }
 
 const opts = {
@@ -44,13 +44,13 @@ const opts = {
     linux: uri + "libflowscripter_template_deno_rust_library.so",
   },
   policy: CachePolicy.NONE,
-};
+}
 const _lib = await prepare(opts, {
   add: { parameters: ["i32", "i32"], result: "i32", nonblocking: false },
-});
+})
 
 export function add(a0: number, a1: number) {
-  let rawResult = _lib.symbols.add(a0, a1);
-  const result = rawResult;
-  return result;
+  let rawResult = _lib.symbols.add(a0, a1)
+  const result = rawResult
+  return result
 }
